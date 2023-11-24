d4c63029b007b50c0f37592328c021ad629934a5
Zach Willis <zachmw27@gmail.com>
2023-11-24 17:24:50 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(event);
+
+        console.log(input.value);
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector("section#movieDetials h4");
+                const summary = document.querySelector("section#movieDetails p");
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
